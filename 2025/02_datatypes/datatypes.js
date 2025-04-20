c1e4b9ae5f59c271c1a5c611b07263771651bc54
aryantapre <taprearyan7@gmail.com>
2025-04-20 18:04:34 +0530

    add datatypes

---
+++ 2025/02_datatypes/datatypes.js
@@ -0,0 +1,57 @@
+"use strict";
+// https://javascript.info/types
+
+
+// there are 8 basic datatypes in javascript language...
+
+// Number
+    let age = 22;  // max (2^53-1)  
+
+// bigInt
+    let invoiceNumber = 12121231232323n; // arbitary values
+
+// string
+    let firstName = "aryan";
+    let lastName = "tapre";
+
+// boolean
+    let isGoogleAuthComplete = true; 
+    let isGreaterNo = 4 > 1;
+
+// null
+    let shoePrices = null;
+
+// undefined
+    let address; // Default is "undefined"
+
+// symbol 
+    let goa = Symbol("1x24pAb"); // creates Unique identifier for Object.
+
+// Non-primitive Object
+    let books = {
+        name:   "Elon Musk by Walter issacson",
+        price:  `300$`
+    }
+
+    console.log(books);
+    
+
+
+// typeof 
+console.log(typeof 10);
+console.log(typeof 100n);
+console.log(typeof "aruan");
+console.log(typeof false);
+console.log(typeof null); // null is not as object
+console.log(typeof undefined);
+console.log(typeof Symbol);
+console.log(typeof Object);
+console.log(typeof alert);
+
+
+
+
+
+
+
+
