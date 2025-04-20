63a2fa4c6018904e31485c65fa969de8b46a9a5a
aryantapre <taprearyan7@gmail.com>
2025-04-20 20:39:33 +0530

    type conversion

---
+++ 2025/04_typeConversions/typeConversion.js
@@ -0,0 +1,21 @@
+// NOTE: String Conversion
+let value = true;
+value = String(value);
+console.log(typeof value);
+
+// NOTE: Numeric Conversion
+let strValue = "1";
+strValue = Number(strValue);
+console.log(strValue + 10);
+console.log(typeof strValue);
+
+
+// NOTE: Boolean Conversion
+console.log(Boolean(1)); //         true
+console.log(Boolean(0)); //         false
+console.log(Boolean("hello")); //   true
+console.log(Boolean("")); //        false
+
+
+
+ 
