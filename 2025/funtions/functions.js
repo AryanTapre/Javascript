631582a27e6b1f05dcb22bcff0f19e5c5fecd3e3
aryantapre <taprearyan7@gmail.com>
2025-04-21 01:39:13 +0530

    functions

---
+++ 2025/funtions/functions.js
@@ -0,0 +1,10 @@
+// https://javascript.info/function-expressions#function-is-a-value
+
+//NOTE: Function declaration.
+
+greet();
+
+const greet = () => {        
+    console.log("Good morning");    
+}
+
