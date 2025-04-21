7b7f2e60d3a629b4b77beb18c93c00380915683b
aryantapre <taprearyan7@gmail.com>
2025-04-21 22:25:56 +0530

    function-declaration,expression,arrow

---
+++ 2025/06_functions/function.js
@@ -0,0 +1,12 @@
+// NOTE: Default function declaration
+function calcSum(a, b) {
+    return a + b;
+}
+
+// NOTE: Function Expression
+const calcSum2 = function(a, b) {
+    return a + b;
+}
+
+// NOTE: Arrow functions
+const calcSum3 = (a, b) => a + b;
