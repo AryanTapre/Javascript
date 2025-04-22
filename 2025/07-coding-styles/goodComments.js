0b8276adda06e8ed1045b13fef97c57a22df5c45
aryantapre <taprearyan7@gmail.com>
2025-04-22 14:37:07 +0530

    good comment styles

---
+++ 2025/07-coding-styles/goodComments.js
@@ -0,0 +1,12 @@
+
+/**
+ * Returns x raised to the n-th power.
+ * 
+ * @param {number} x The number to raise. 
+ * @param {number} n The power, must be natural number. 
+ * @returns {number} x raised to tbe n-th power
+ */
+function pow(x, n) {
+    return x ** n;
+}
+
