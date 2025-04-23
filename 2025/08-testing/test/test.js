6d64cabd21252288d449a5f7a21433f29df70107
aryantapre <taprearyan7@gmail.com>
2025-04-23 12:35:26 +0530

    test done mocha + chai

--- 2025/08-testing/test/test.js
+++ 2025/08-testing/test/test.js
@@ -6,6 +6,15 @@
 
 describe("pow", function() {
     function generateTestCases(x) {
+
+        it("for negative n the result is NaN", function() {
+            assert.isNaN( pow(x, -1));
+        })
+
+        it("for non-integer n the result is NaN", function() {
+            assert.isNaN( pow(x, 1.5));
+        })
+
         let expected = x * x * x;
         it(`${x} raise to power 3 is ${expected}`, function() {
             assert.equal( pow(x, 3), expected)
@@ -16,3 +25,5 @@ describe("pow", function() {
         generateTestCases(x);
     }
 });
+
+str
