ff5f73b141e875d4379b2e6dec6ded0091d6da44
aryantapre <taprearyan7@gmail.com>
2025-04-25 16:24:09 +0530

    optional chaining

---
+++ 2025/10-objects/05-optional-chaining.js
@@ -0,0 +1,36 @@
+//NOTE: https://javascript.info/optional-chaining
+
+const UserStatus = function() {
+    this.name = "aryan";
+    this.address = {
+        street: null,
+        area: null,
+    };
+    this.isAuth = {
+        google: {
+            value: null
+        },
+        facebook: null,
+        x: null,
+        github: null,
+        microsoft: null,
+    };
+}
+
+const userStatus = new UserStatus();
+console.log(userStatus);
+console.log(userStatus.isAuth?.google?.value ?? undefined);
+
+
+
+const user = {
+    profile: {
+        name: "Aryan",
+        social: {
+            twitter: "@aryant_x"
+        }
+    }
+};
+
+const instagram = user.profile?.social.instagram ?? "Not Linked";
+console.log(instagram);
