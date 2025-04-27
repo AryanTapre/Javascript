70757d3bb2cc0df39d51a0171608496eda748b59
aryantapre <taprearyan7@gmail.com>
2025-04-27 23:20:43 +0530

    array todo

--- 2025/11-primitive-objects/04-array.js
+++ 2025/11-primitive-objects/04-array.js
@@ -59,3 +59,77 @@ for (let i = 0; i < matrix.length; i++) {
     }
     console.log();   
 }
+
+for (const [x, y] of arr2.entries()) {
+    console.log(x + " : " + y);
+}
+
+let arr3 = [12, NaN,"20"];
+const data = arr3.every((value, index, array) => {
+    if(typeof value === "string") {
+        console.log("inside good");
+        
+        return "good";
+    }else {
+        console.log("inside bad");
+        
+        return "bad";
+    }
+});
+
+console.log(data);
+
+
+//console.log(arr3.fill("x"));
+
+// TODO: FILTER
+const datax = arr3.filter((value, index, array) => {
+    return typeof value == "string";
+});
+console.log(datax);
+
+console.log("find() ==> \n");
+
+const result = arr3.find((value, index, array) => {
+    return typeof value === "string";
+});
+console.log(result);
+
+
+// TODO: MAP
+console.log("map() ==> \n");
+console.log(arr3);
+
+
+const newArray3 = arr.map((value, index, array) =>  {
+    console.log(this);
+})
+
+
+const numberArray = [1, 2, 3, 4, 5];
+
+const sum = numberArray.reduce((accumulate, current, index, array) => {
+    accumulate = current + accumulate;
+    return accumulate;
+},0) 
+console.log(sum);
+
+
+//TODO: 1 Let’s try 5 array operations.
+/*
+    1.  Create an array styles with items “Jazz” and “Blues”.
+    2.  Append “Rock-n-Roll” to the end.
+    3.  Replace the value in the middle with “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+    4.  Strip off the first value of the array and show it.
+Prepend Rap and Reggae to the array. 
+*/
+
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+console.log(styles);
+
+function replaceMiddleElement(arr, replaceString) {
+    arr[Math.ceil((arr.length - 1) / 2)] = replaceString;
+}
+replaceMiddleElement(styles, "arya");
+console.log(styles);
