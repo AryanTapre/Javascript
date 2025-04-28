ed8d17f2946a4577f01abf99ddadde9d715054df
aryantapre <taprearyan7@gmail.com>
2025-04-28 22:06:42 +0530

    grinded array methods

--- 2025/11-primitive-objects/04-array.js
+++ 2025/11-primitive-objects/04-array.js
@@ -138,4 +138,114 @@ console.log(styles);
 
 //---------------------------------------------METHODS-----------------------------------------
 
+//NOTE: ADD / REMOVE ITEMS
 
+const tesla = ["model-x","model-y","model-s","roadster"];
+tesla.push("CyberTruck");
+console.log(tesla);
+tesla.pop();
+console.log(tesla);
+
+tesla.shift();
+console.log(tesla);
+tesla.unshift("model-x");
+console.log(tesla);
+
+
+// IMPORTANT: Splice
+console.log(tesla.splice(1,1,"aryan")); // replacing tesla[1] with "aryan" as string data-type
+console.log(tesla);
+
+// IMPORTANT: Slice
+console.log(tesla.slice(0, 2)); // Copied element from index 0 to 2
+
+//IMPORTANT: Concat
+console.log( tesla.concat(["elonmusk", "spaceX"]) ); // appended "elonmusk" and "spaceX" to tesla array DOES NOTE CHANGE ORIGINAL ARRAY RETURN THE MODIFIED ONE...
+
+
+// IMPORTANT: For..Each loop
+
+tesla.forEach((value, index, array) => {
+    console.log(value + ":" + index + ":"); 
+});
+
+
+// NOTE: SEARCHING IN AN ARRAY..
+
+// IMPORTANT: indexOf() , lastIndexOf() , include()
+
+console.log(tesla);
+
+console.log( tesla.indexOf("model-s") );
+console.log( tesla.lastIndexOf("roadster") );
+console.log( tesla.includes("model-x") );
+
+// IMPORTANT: find(), findIndex(), findLastIndex()
+console.log( tesla.find((value, index, array) => value === "model-x" ) );
+console.log( tesla.findIndex((value, index, array) => value === "model-x" ) );
+console.log( tesla.findLastIndex((value, index, array) => value === "model-x" ) );
+
+// IMPORTANT: filter()
+
+console.log( tesla.filter((value, index, array) =>  typeof value == "string" ));
+
+
+// NOTE: TRANSFORM AN ARRAY
+
+// IMPORTANT: map()
+
+console.log( tesla.map((value, index, arr) => value.toUpperCase() ) ); // modifies the array and returns,but original remains the same as before...
+console.log(tesla);
+
+// IMPORTANT: Sort()
+
+let xcus = [1, 15, 2];
+xcus.sort((a, b) => { return a - b }); // custom comparison function
+console.log(xcus);
+
+// IMPORTANT: reverse()
+xcus.reverse();
+console.log(xcus);
+
+// IMPORTANT: split(), and join()
+
+let apple = "iPhone, Mac book pro, Mac book pro air, Mac Mini";
+let splitted = apple.split(", "); // Spliting a single string into array
+console.log(splitted);
+
+let joined = splitted.join(", ");
+console.log(joined);
+
+// IMPORTANT reduce()
+
+// finding the sum of 'xcus'...
+console.log(xcus);
+
+console.log( xcus.reduce((accumulator, value, index, array) => {
+    return accumulator += value;    
+}, 0) );
+
+
+
+/** FIXME:
+ * Most methods support “thisArg”
+Almost all array methods that call functions – like find, filter, map, with a notable exception of sort, accept an optional additional parameter thisArg.
+ */
+
+/** TODO: que
+ *  create object named 'appleIphone' with there iPhones
+ *  create array appleUser with name of person
+ *  iterate over 'sppleUser' array and logs the username with iphone
+ */
+
+let appleIphone = {
+    iPhone13: "iPhone13",
+    iPhone13_mini: "iPhone13-mini",
+    iPhone15: "iPhone15"
+};
+
+let appleUsers = ["aryan", "panvi", "harshad"];
+
+appleUsers.forEach(function(value, index, array) {
+    console.log(value + ":"+this.iPhone13_mini);
+}, appleIphone);
