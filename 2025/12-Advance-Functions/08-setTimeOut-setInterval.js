1cfa79e17e6ed5e5fed6c14d0a77f0b5afc1783f
aryantapre <taprearyan7@gmail.com>
2025-05-02 19:01:10 +0530

    setTimeOut and setInterval

---
+++ 2025/12-Advance-Functions/08-setTimeOut-setInterval.js
@@ -0,0 +1,39 @@
+//NOTE: https://javascript.info/settimeout-setinterval
+
+const timerId1 = setTimeout(function(...data) {
+    console.log("timer 1 called");
+    console.log(data);
+    
+    
+}, 5, "aryan","tapre");
+
+console.log("timerId1 : ", timerId1);
+
+//clearTimeout(timerId1);   IMPORTANT: TO CANCEL SetTimeOut call
+
+
+//NOTE: WORKING WITH setInterval() function
+
+const timerId2 = setInterval(function() {
+    console.log("Sending request to the server,,,");
+    
+}, 100);
+
+clearInterval(timerId2);
+
+
+// find deplays between Intervals
+let start = Date.now();
+let times = [];
+
+setTimeout(function run() {
+    times.push( Date.now() - start );
+    
+    if (start + 100 < Date.now()) {
+        console.log(times);
+    } else {
+        setTimeout(run, 0);
+    }
+},0)
+
+
