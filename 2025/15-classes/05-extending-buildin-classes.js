710993e964655f8b8ea14e0dbff56f58282d3c94
aryantapre <taprearyan7@gmail.com>
2025-05-06 09:52:40 +0530

    extending buildin classes

---
+++ 2025/15-classes/05-extending-buildin-classes.js
@@ -0,0 +1,22 @@
+// NOTE: https://javascript.info/extend-natives
+
+class PowerArray extends Array {
+    isEmpty() {
+        return this.length === 0;   
+    }
+};
+
+const arr = new PowerArray();
+arr.push("aryan");
+arr.unshift("tapre");
+
+console.log(arr.isEmpty());
+
+console.log(arr.constructor == PowerArray);
+
+
+
+class Custom extends Object {
+
+};
+
