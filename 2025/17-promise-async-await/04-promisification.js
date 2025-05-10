feb071ba4a67022b7804a0d9141bdb2757a944a0
aryantapre <taprearyan7@gmail.com>
2025-05-10 18:27:25 +0530

    promisification

---
+++ 2025/17-promise-async-await/04-promisification.js
@@ -0,0 +1,36 @@
+//NOTE: https://javascript.info/promisify
+
+// function callback() {
+//     console.log(`The name is : ${this.name}`);
+// }
+
+// function main(name, callback) {
+//     this.name = name;
+//     callback();
+// }
+
+// main("Aryan", callback);
+
+
+function promisify(func) {
+    return function(...args) {
+        return new Promise((resolve, reject) => {
+            function callback() {
+                console.log(`The data is : ${this.name}`);   
+            }
+
+            args.push(callback);
+            func.call(this, ...args);
+            resolve(this.name)
+        });    
+    };
+}
+
+function main(name, callback) {
+    this.name = name;
+    callback();
+}
+
+
+const proMain = promisify(main);
+proMain("AryanTapre").then((d) => console.log("x:" + d))
