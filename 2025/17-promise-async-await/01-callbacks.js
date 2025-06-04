73de83efbb7a3a3f8d79ba0d4765150ea0e525f9
aryantapre <taprearyan7@gmail.com>
2025-06-05 01:53:58 +0530

    Promise

--- 2025/17-promise-async-await/01-callbacks.js
+++ 2025/17-promise-async-await/01-callbacks.js
@@ -1,17 +1,47 @@
 // NOTE: https://javascript.info/callbacks
 
-function loadData(name, callback) {
-    this.name = name;
-    Object.hasOwn(this, "name") ? callback(null, this.name) : callback("failed to access property", null);
+// function loadData(name, callback) {
+//     this.name = name;
+//     Object.hasOwn(this, "name") ? callback(null, this.name) : callback("failed to access property", null);
+// }
+
+// loadData("frontman", function (error, result) {    
+//     if (error) {
+//         console.log(`there was an error : ${error}`);   
+//     }
+//     else {
+//         console.log(`the data is : ${result}`);
+//     }
+// });
+
+
+//+======++++++++==== REAL WORLD =============================
+
+function fetchData(url, callback) {
+    console.log("...inside fetchData() function...");
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => callback(null, data))
+        .catch(err => callback(err, null))
+
 }
 
-loadData("frontman", function (error, result) {    
-    if (error) {
-        console.log(`there was an error : ${error}`);   
-    }
-    else {
-        console.log(`the data is : ${result}`);
+fetchData("https://api.github.com/users/AryanTapreeerr", (err, data) => {
+
+    console.log("... inside fetchData() callback");
+    
+    if (err) {
+        console.log(`GOT THE ERROR: ${err}`);        
+    } else {
+        console.log(`GOT THE DATA: ${data}`);
     }
 });
-E
 
+
+console.log("aryan tapre");
+
+function hola() {
+    console.log("hola to you sir...");
+} 
+hola();
--- 2025/17-promise-async-await/02-promise.js
+++ 2025/17-promise-async-await/02-promise.js
@@ -27,3 +27,66 @@ results
     .then((data) => console.log("Got data: "+ data))
     .catch((error) => console.log("Got Error: "+ error))
 
+
+//================== UNDERSTADING PROCMISES=======================
+
+
+// function getUsers(url, callback) {
+//     setTimeout(() => {
+//         console.log("...got users from remote host... ", url);
+//         callback(null, {name: "aryan tapre", age: 22});
+//     },1000);
+// }
+
+// getUsers("https://google.com/users/", function notifyUsers(err, data) {
+//     if (err) {
+//         console.log("ERROR_OCCURED: ", err);        
+//     } else if (data) {
+//         console.log("RECEIVED DATA: ", data);
+//     }
+// });
+
+
+function getUsers(url) {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve( {name: "aryantapre", age:22, url} );
+        },1000); 
+    });
+    
+    return promise;
+}
+
+getUsers("https://google.com/users")
+    .then((data) => console.log(`RECEIVED DATA: ${data}`))
+    .catch((err) => console.error(`RECEIVED ERROR : ${err}`))
+
+
+
+
+
+//=========== HANDLING REAL-WORLD-DATA FROM GITHUB====================
+
+function getGithubUser(username) {
+    const url = `https://api.github.com/users/${username}`;
+
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Github API ERROR: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(`Github User Data Received: ${JSON.stringify(data, null, 2)}`);
+            // for (key in data) {
+            //     console.log(`${key} : ${data[key]}`);
+                
+            // }
+        })
+        .catch((error) => {
+            console.log(`Faild to fetch Github User Data: ${error}`);
+        })
+}
+
+getGithubUser("AryanTapre");
