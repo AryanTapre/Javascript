b6be75eae9db436d34f414db4b5736e26957ba2b
aryantapre <taprearyan7@gmail.com>
2025-06-07 22:10:24 +0530

    modules

---
+++ 2025/20-modules/dynamic-binding/orchestrator.mjs
@@ -0,0 +1,6 @@
+import {value, getValue, setValue} from "./tools.mjs";
+
+console.log("value : ", value);
+setValue(100);
+console.log("new value:", value);
+
---
+++ 2025/20-modules/dynamic-binding/tools.mjs
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log(globalThis);
+
+// let value = 10;
+
+// function setValue(newVal) { value = newVal }
+
+// function getValue(newVal) { return value }  
+
+
+// export {value, setValue, getValue};
+// value += 1;
